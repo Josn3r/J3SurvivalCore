0da1f1c2c6718827d9cdc9e75a70b7a06cd584bc
josne <josne@DESKTOP-R9RKFN9>
2023-08-19 22:23:19 -0400

    Updated

---
+++ src/store/j3studios/plugin/SCore.java
@@ -0,0 +1,11 @@
+package store.j3studios.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SCore extends JavaPlugin {
+    
+    public void onEnable() {
+        
+    }
+    
+}
